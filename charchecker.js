1fb6a4102cbc032be7f1f4278f8d7655450740da
Farheen <farhashaik99@gmail.com>
2025-09-01 23:34:54 -0400

    Added the code to character checker

--- charchecker.js
+++ charchecker.js
@@ -5,10 +5,10 @@ const readlineSync = require("readline-sync");
 let inputString = readlineSync.question("Enter a string as input\n");
 
 //Prompt a user for an index number to find the character at that index
-let indexNumber = readlineSync.question("Enter an index number\n");
+let indexNumber = readlineSync.question("Enter the index number\n");
 
 //Use bracket notation to access the character
 let characterAt = inputString[indexNumber];
 
 //Print out the character
-console.log("Character at the index number" +indexNumber + " is "+  characterAt);
+console.log("Character at the index number " + indexNumber + " is: " + characterAt);
